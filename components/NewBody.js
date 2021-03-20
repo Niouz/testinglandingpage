e771aace3205b81ae904ab0c13d28833660499b4
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2021-03-20 15:15:23 -0400

    i am tired

--- components/NewBody.js
+++ components/NewBody.js
@@ -9,11 +9,11 @@ const NewBody = ( { logo, interfaceNiouzImage }) => {
                     <img src={logo} alt="nioz Logo" />
                     <span>&#9776;</span>
                </div>
-               <p>Democratizing news for all <strong>Haitians.</strong></p>
+               <h3>Democratizing news for all <strong>Haitians.</strong></h3>
            </div>
 
            <div className={styles.spot}>
-               <h3>Spot on news happening in the streets </h3>
+               <h4>Spot on news happening in the streets </h4>
                <p>Get all the news on your area and engage in those that matter to you. </p>
                <img src={interfaceNiouzImage} />
            </div>
