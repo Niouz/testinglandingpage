8528b8123f1eecc4af539cb24dab70d9ce7184e6
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2021-03-20 12:17:34 -0400

    Add files via upload

--- components/NewFooter.js
+++ components/NewFooter.js
@@ -27,7 +27,7 @@ const NewFooter = ( {logo, appStore, playStore, facebook, twitter, instagram} ) 
             </div>
 
             <div className={styles.copyright}>
-                <p>© Copyright 2021 Niouz, all rights reserved</p>
+                <p>© Copyright { new Date().getFullYear()} Niouz, all rights reserved</p>
             </div>
         </footer>
 
