c6d1e0edcd4b2371da6faa7a4b509631d1a0dad6
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2021-03-20 10:53:30 -0400

    Add files via upload

--- components/NewFooter.js
+++ components/NewFooter.js
@@ -8,11 +8,10 @@ const NewFooter = ( {logo, appStore, playStore, facebook, twitter, instagram} ) 
             <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
             <div className={styles.logo}>
                 <img src={logo} alt="Logo" />
-                <p>Democratizing niews for all Haitians</p>
+                <p>Democratizing news for all Haitians</p>
             </div>
 
             <div className={styles.media}>
-                <p> Follow Us </p>
                     <a><i className="fab fa-facebook"></i></a>
                     <a><i className="fab fa-twitter"></i></a>
                     <a><i className="fab fa-linkedin"></i></a>  
@@ -21,8 +20,10 @@ const NewFooter = ( {logo, appStore, playStore, facebook, twitter, instagram} ) 
 
             <div className={styles.download}>
                 <p> Download </p>
-                <img src={appStore} />
-                <img src={playStore} />
+                    <div>
+                        <img src={appStore} />
+                        <img src={playStore} /> 
+                    </div>
             </div>
 
             <div className={styles.copyright}>
