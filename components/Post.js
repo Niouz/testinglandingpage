1b5b9f9dfae3b8d87b33cbf1ce335f95b4be3117
jasneljuillet <jasneljuillet1995@gmail.com>
2021-03-31 12:29:52 -0400

    post

--- components/Post.js
+++ components/Post.js
@@ -11,7 +11,7 @@ const Post = ( { title, body, photo, source } ) => {
             </header>
 
             <div className="flex z-0">
-                <img className=" w-full" src ="./image.jpg" />
+                <img className=" w-full h-auto object-cover" src ="./image.jpg" />
             </div>
 
             <div className=" w-full md:text-lg flex bg-secondary-100 " >
