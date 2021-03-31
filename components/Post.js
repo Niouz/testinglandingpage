d10408022e786a2d894a9d7ec48d87b7473cbef4
jasneljuillet <jasneljuillet1995@gmail.com>
2021-03-31 14:26:30 -0400

    post

--- components/Post.js
+++ components/Post.js
@@ -14,10 +14,8 @@ const Post = ( { title, body, photo, source } ) => {
                 <img className=" object-fill lg:h-auto md:h-auto h-88 w-full " src ="./image.jpg" />
 
                 <div className="bg-secondary-100 text-secondary-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2">
-                    <svg className="w-4 text-primary inline-block" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" 
-                    xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" 
-                    stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <span className="text-primary"> 5 Mins ago</span>
+                <svg class="w-5 text-primary inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span className="text-primary hover:shadow-inner"> 5 Mins ago</span>
                 </div>
             </div>
 
