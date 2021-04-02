267fcca0b0790beef174d105f1d76236e22f09fa
jasneljuillet <jasneljuillet1995@gmail.com>
2021-04-02 15:18:04 -0400

    post component

--- components/Post.js
+++ components/Post.js
@@ -1,32 +1,35 @@
+import tw from 'twin.macro';
+
 import Header from "./Header";
 import NewFooter from "./NewFooter";
 
+
 const Post = ( { title, body, photo, source } ) => {
     return(
-        <main className="">
+        <main tw="">
             <Header />
             
-            <header className="flex bg-secondary-100 px-2 py-4 w-full h-16"> 
-                <img className="w-20 " src = "./logo.png" /> 
+            <header tw="flex bg-secondary-100 px-2 py-4 w-full h-16"> 
+                <img tw="w-20 " src = "./logo.png" /> 
             </header>
 
-            <div className="shadow-md relative">
-                <img className=" object-fill lg:h-auto md:h-auto h-88 w-full " src ="./image.jpg" />
+            <div tw="shadow-md relative">
+                <img tw=" object-fill lg:h-auto md:h-auto h-88 w-full " src ="./image.jpg" />
 
-                <div className="bg-secondary-100 text-secondary-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2">
-                <svg class="w-4 text-primary inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <span className="text-primary"> 5 Mins ago</span>
+                <div tw="bg-secondary-100 text-secondary-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2">
+                <svg tw="w-4 text-primary inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span tw="text-primary"> 5 Mins ago</span>
                 </div>
             </div>
 
-            <div className=" w-full md:text-lg flex bg-secondary-100 " >
-                <h2 className="text-primary text-center font-bold sm:text-xl md:p-4 p-4 ">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+            <div tw=" w-full md:text-lg flex bg-secondary-100 " >
+                <h2 tw="text-primary text-center font-bold sm:text-xl md:p-4 p-4 ">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
                     standard dummy text ever since the 1500s
                  </h2>
             </div>
 
-            <div className="w-full lg:p-20 md:p-12 p-8  text-justify">
-                <p className="">
+            <div tw="w-full lg:p-20 md:p-12 p-8  text-justify">
+                <p tw="">
                 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
  it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic
@@ -46,7 +49,7 @@ const Post = ( { title, body, photo, source } ) => {
    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
    including versions of Lorem Ipsum    
                 </p>
-                <p className=" flex justify-end mt-4">Source</p>
+                <p tw=" flex justify-end mt-4">Source</p>
             </div>
             
             <NewFooter 
