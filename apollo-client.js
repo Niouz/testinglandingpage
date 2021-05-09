b88927d17294e7410f766a06df4202b6feea26f0
jasneljuillet <jasneljuillet1995@gmail.com>
2021-05-09 11:19:41 -0400

    Update appolo client uri

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://niouzapp.com/api",
+    uri: "https://api.niouzapp.com/api",
     cache: new InMemoryCache(),
 });
 
